2fa74d8dbe8c07cc9e41dce5d511f74a4fd26fa9
ralfy8 <ralfymarvie@gmail.com>
2026-03-05 10:50:01 +0100

    Add files via upload

---
+++ funciones/ejercicio1.py
@@ -0,0 +1,9 @@
+def es_positivo(numero):
+    return numero > 0
+
+# Pruebas de la función
+print(es_positivo(10))    # True
+print(es_positivo(-3))    # False
+print(es_positivo(0))     # False
+print(es_positivo(2.5))   # True
+print(es_positivo(-0.1))  # False
